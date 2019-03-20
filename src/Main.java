6b42be4905a86a91ef267026fa7b486f48d5a8b8
LingdaLi <leanlee311@gmail.com>
2019-03-19 22:19:41 -0700

    Remove test cases in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 public class Main {
 
-    public static void main(String[] args) {
-        FindKthSmallestPair solution = new FindKthSmallestPair();
-        int[] nums = {1 , 3, 1};
-        solution.smallestDistancePair(nums, 1);
-    }
+    public static void main(String[] args) {}
 }
